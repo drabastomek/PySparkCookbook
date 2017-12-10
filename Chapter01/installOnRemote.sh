72f1cc2b94416873f703b8489e4ba3f719767dd6
Tomek Drabas <drabas.t@gmail.com>
2017-12-10 00:18:02 -0800

    Fully working installOnRemote.sh script

--- Chapter01/installOnRemote.sh
+++ Chapter01/installOnRemote.sh
@@ -12,9 +12,12 @@ _spark_binary="http://mirrors.ocf.berkeley.edu/apache/spark/spark-2.2.0/spark-2.
 _spark_archive=$( echo "$_spark_binary" | awk -F '/' '{print $NF}' )
 _spark_dir=$( echo "${_spark_archive%.*}" )
 _spark_destination="/usr/local/spark"
+_java_destination="/usr/lib/jvm/java-8-oracle"
 
 _machine=$(cat /etc/hostname)
 _today=$( date +%Y-%m-%d )
+
+_current_dir=$(pwd) # store current working directory
 
 function printHeader() {
     echo
@@ -78,6 +81,46 @@ function readIPs() {
     done < "$input"
 }
 
+function checkJava() {
+    echo
+    echo "##########################"
+    echo
+    echo "Checking Java"
+    echo
+
+    if type -p java; then
+        echo "Java executable found in PATH"
+        _java=java
+    elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+        echo "Found Java executable in JAVA_HOME"
+        _java="$JAVA_HOME/bin/java"
+    else
+        echo "No Java found. Install Java version $_java_required or higher first or specify JAVA_HOME variable that will point to your Java binaries."
+        installJava
+    fi
+}
+
+function installJava() {
+    echo
+    echo "##########################"
+    echo
+    echo "Installing Java"
+    echo
+    sudo apt-get install python-software-properties
+    sudo add-apt-repository ppa:webupd8team/java
+    sudo apt-get update
+    sudo apt-get install oracle-java8-installer
+}
+
+function installScala() {
+    echo
+    echo "##########################"
+    echo
+    echo "Installing Scala"
+    echo
+    sudo apt-get install scala
+}
+
 function updateHosts() {
     echo
     echo "##########################"
@@ -107,18 +150,26 @@ function updateHosts() {
 }
 
 function configureSSH() {
+    echo
+    echo "##########################"
+    echo
+    echo "Configuring SSH connections "
+    echo
     # # install the Open SSH
-    sudo apt-get install openssh-server openssh-client
+    # sudo apt-get install openssh-server openssh-client
 
     # check if master
     IFS=" "
     read -ra temp <<< "$_masterNode"
     _current=( ${temp[1]} )
+    _all_machines="$_current\n"
 
     if [ "$_current" = "$_machine" ]; then
         # generate key pairs (passwordless)
-        ssh-keygen -t rsa -P "" -f ~/.ssh/id_rsa
+        sudo -u hduser rm -f ~/.ssh/id_rsa && sudo -u hduser ssh-keygen -t rsa -P "" -f ~/.ssh/id_rsa
+        # echo -e 'y\n' | ssh-keygen -q -t rsa -P "" -f ~/.ssh/id_rsa
 
+        IFS="\n"
         # loop through all the slaves
         read -ra temp <<< "$_slaves"
         for slave in ${temp[@]}; do 
@@ -130,19 +181,22 @@ function configureSSH() {
             # split on space
             IFS=" "
             read -ra temp_inner <<< "$slave"
+            echo
+            echo "Trying to connect to ${temp_inner[1]}"
 
             # ssh to the remote node from master
             # create .ssh directory
             # and output the public key to authorized_keys
             # file inside .ssh
-            cat ~/.ssh/id_rsa.pub | ssh "$USER"@"${temp_inner[1]}" 'mkdir -p .ssh && cat >> .ssh/authorized_keys'
+            cat ~/.ssh/id_rsa.pub | ssh "hduser"@"${temp_inner[1]}" 'mkdir -p .ssh && cat >> .ssh/authorized_keys'
 
-            # alter permissions for the .ssh folder and 
-            # for the authorized_keys file
-            ssh "$USER"@"${temp_inner[1]}" "chmod 0700 .ssh; chmod 0640 .ssh/authorized_keys"
+            # append the slave name to the _all_machines
+            # (we'll need it later)
+            _all_machines=$_all_machines"${temp_inner[1]}\n"
         done
-
     fi
+
+    echo "Finishing up the SSH configuration"
 }
 
 # Download the package
@@ -190,6 +244,7 @@ function moveTheBinaries() {
     fi
 
     sudo mv $_spark_dir/ $_spark_destination/
+    sudo chown -R hduser:hadoop $_spark_destination
 
     echo
 }
@@ -201,11 +256,7 @@ function setSparkEnvironmentVariables() {
     echo "Setting Spark Environment variables"
     echo
 
-    if [ "$_machine" = "Mac" ]; then
-        _bash=~/.bash_profile
-    else
-        _bash=~/.bashrc
-    fi
+    _bash=~/.bashrc
 
     # make a copy just in case
     if ! [ -f "$_bash.spark_copy" ]; then
@@ -216,32 +267,48 @@ function setSparkEnvironmentVariables() {
     echo "###################################################" >> $_bash
     echo "# SPARK environment variables" >> $_bash
     echo "#" >> $_bash
-    echo "# Script: installFromSource.sh" >> $_bash
+    echo "# Script: installOnRemote.sh" >> $_bash
     echo "# Added on: $_today" >>$_bash
     echo >> $_bash
 
     echo "export SPARK_HOME=$_spark_destination" >> $_bash
-    echo "export PYSPARK_SUBMIT_ARGS=\"--master local[4]\"" >> $_bash
-    echo "export PYSPARK_PYTHON=$(type -p python)" >> $_bash
-    echo "export PYSPARK_DRIVER_PYTHON=jupyter" >> $_bash
-
-    echo "export PYSPARK_DRIVER_PYTHON_OPTS=\"notebook --NotebookApp.open_browser=False --NotebookApp.port=6661\"" >> $_bash
+    echo "export JAVA_HOME=$_java_destination" >> $_bash
     
     echo "export PATH=$SPARK_HOME/bin:\$PATH" >> $_bash
 }
 
+function updateSparkConfig() {
+    echo
+    echo "##########################"
+    echo
+    echo "Configuring Spark"
+    echo
+    
+    cd $_spark_destination/conf
+
+    sudo -u hduser cp spark-env.sh.template spark-env.sh
+    echo "export JAVA_HOME=$_java_destination" >> spark-env.sh
+    echo "export SPARK_WORKER_CORES=12" >> spark-env.sh
+
+    sudo -u hduser cp slaves.template slaves
+    printf "$_all_machines" >> slaves
+}
+
 # Clean up
 function cleanUp() {
-    cd ..
+    cd $_current_dir
     rm -rf _temp
 }
 
 printHeader
 readIPs
-# updateHosts
+checkJava
+installScala
+updateHosts
 configureSSH
-# downloadThePackage
-# unpack
-# moveTheBinaries
-# setSparkEnvironmentVariables
-# cleanUp
+downloadThePackage
+unpack
+moveTheBinaries
+setSparkEnvironmentVariables
+updateSparkConfig
+cleanUp
